6a96b25cad0c12f25f52c4421559f7d0f02cce74
pejmans21 <pezhmansamadi21@gmail.com>
2023-07-21 12:47:18 +0330

    Password utility
    Password hashing & password verification

---
+++ fastapi_user_management/tools/encryption.py
@@ -0,0 +1,29 @@
+"""Encrypt password."""
+from passlib.context import CryptContext
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    """Check password with hashed password.
+
+    Args:
+        plain_password (str): plain text password
+        hashed_password (bool): encrypted password
+
+    Returns:
+        bool: password match or not?
+    """
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def get_password_hash(password: str) -> str:
+    """Get plain password and hash woth bcrypt.
+
+    Args:
+        password (str): plain password
+
+    Returns:
+        str: hashed password
+    """
+    return pwd_context.hash(password)
